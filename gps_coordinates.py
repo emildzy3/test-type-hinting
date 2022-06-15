87decb221b90929738b75772873d2bc6beb7aab8
emildzy <emildzy3@yandex.ru>
2022-06-15 09:15:23 +0300

    Written logic to get GPS

--- gps_coordinates.py
+++ gps_coordinates.py
@@ -1,5 +1,9 @@
-
 from dataclasses import dataclass
+from typing import Literal
+
+import requests
+
+from exception import CantGetCoordinates
 
 
 @dataclass(slots=True, frozen=True)
@@ -12,6 +16,44 @@ def get_coordinate() -> Coordinate:
     """
     Return GPS coordinate current location
     """
-    return Coordinate(latitude=5, longitude=6)
+    coordinates = _get_coordinates_from_api()
+    return coordinates
 
 
+def _get_coordinates_from_api() -> Coordinate:
+    raw_data_from_api = _get_data_api()
+    return _parse_coordinates(raw_data_from_api)
+
+
+def _get_data_api() -> bytes:
+    source_data_coordinates = 'http://ipinfo.io/'
+    server_request = requests.get(source_data_coordinates)
+    if not server_request.ok:
+        raise CantGetCoordinates
+    return server_request.content
+
+
+def _parse_coordinates(raw_data_from_api: bytes) -> Coordinate:
+    try:
+        normalize_data = raw_data_from_api.decode().strip().lower().split('\n')
+    except UnicodeDecodeError:
+        raise CantGetCoordinates
+    return Coordinate(
+        latitude=_parse_coordinate(normalize_data, 'latitude'),
+        longitude=_parse_coordinate(normalize_data, 'longitude')
+    )
+
+
+def _parse_coordinate(normalize_data: list[str],
+                      type_coordinate: Literal['latitude'] | Literal['longitude']) -> float:
+    number_coordinate = 1
+    if type_coordinate == 'latitude':
+        number_coordinate = 0
+    try:
+        return float(normalize_data[6].split()[1].split(',')[number_coordinate].strip('"'))
+    except ValueError:
+        raise CantGetCoordinates
+
+
+if __name__ == "__main__":
+    print(get_coordinate())
